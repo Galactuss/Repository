3e0c5093e5f5a6aa98ffea3e44f7f0a78f0694cf
Galactuss <bholepushpak@gmail.com>
2016-09-04 21:12:22 +0530

    Added hattrick field

--- ISL/src/com/isl/model/MatchPlayer.java
+++ ISL/src/com/isl/model/MatchPlayer.java
@@ -1,9 +1,9 @@
 package com.isl.model;
 
 public class MatchPlayer {
-	
+
 	private int runs_scored = 0;
-	private int balls_faced= 0;
+	private int balls_faced = 0;
 	private int four_scored = 0;
 	private int six_scored = 0;
 	private int runs_given = 0;
@@ -13,7 +13,16 @@ public class MatchPlayer {
 	private boolean half_century = false;
 	private boolean century = false;
 	private boolean isNotOut = false;
-	
+	private int hattrickCount = 0;
+
+	public int getHattrickCount() {
+		return hattrickCount;
+	}
+
+	public void setHattrickCount(int hattrickCount) {
+		this.hattrickCount = hattrickCount;
+	}
+
 	public boolean isNotOut() {
 		return isNotOut;
 	}
@@ -49,55 +58,55 @@ public class MatchPlayer {
 	public int getRuns_scored() {
 		return runs_scored;
 	}
-	
+
 	public void setRuns_scored(int runs_scored) {
 		this.runs_scored = runs_scored;
 	}
-	
+
 	public int getBalls_faced() {
 		return balls_faced;
 	}
-	
+
 	public void setBalls_faced(int balls_faced) {
 		this.balls_faced = balls_faced;
 	}
-	
+
 	public int getFour_scored() {
 		return four_scored;
 	}
-	
+
 	public void setFour_scored(int four_scored) {
 		this.four_scored = four_scored;
 	}
-	
+
 	public int getSix_scored() {
 		return six_scored;
 	}
-	
+
 	public void setSix_scored(int six_scored) {
 		this.six_scored = six_scored;
 	}
-	
+
 	public int getRuns_given() {
 		return runs_given;
 	}
-	
+
 	public void setRuns_given(int runs_given) {
 		this.runs_given = runs_given;
 	}
-	
+
 	public int getBalls_bowled() {
 		return balls_bowled;
 	}
-	
+
 	public void setBalls_bowled(int balls_bowled) {
 		this.balls_bowled = balls_bowled;
 	}
-	
+
 	public int getWickets_taken() {
 		return wickets_taken;
 	}
-	
+
 	public void setWickets_taken(int wickets_taken) {
 		this.wickets_taken = wickets_taken;
 	}
